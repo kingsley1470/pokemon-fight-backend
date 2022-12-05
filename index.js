1abde4a821c29896b4231c4fd8953dc63a25cd91
kingsley <rawmetal77@gmail.com>
2022-12-05 11:03:47 +0100

    backend file

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const pokemonData = require("./pokemonData.json");
+const getPokemonById = require("./route/controller");
+
+const app = express();
+
+
+
+
+app.get("/pokemon", (req, res) => {
+  res.send(pokemonData);
+});
+
+app.get("/pokemon/:id", (req, res) => {
+  res.status(200).send(getPokemonById(parseInt(req.params.id), pokemonData));
+});
+
+app.get("/pokemon/:id/:info", (req, res) => {
+  const { id, info } = req.params;
+  const getInfo = getPokemonById(parseInt(id), pokemonData)[info];
+  res.status(200).send(getInfo);
+});
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => console.log(`server started on port${PORT}`));
